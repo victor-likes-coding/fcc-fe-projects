c173bf6f651129746ae078ea4e5f4be47899b5f6
Victor Tran <victor.tran@tempestas.io>
2021-05-11 22:01:26 -0400

    add ids because fcc project requirement

--- src/App.js
+++ src/App.js
@@ -6,14 +6,29 @@ const App = ({ selected, getNewQuote }) => {
   return (
     <div className="App">
       <div className="container">
-        <div className="quote-container">
-          <section className="quote">{quote}</section>
-          <section className="author">&mdash; {author}</section>
-          <section className="social-media"></section>
-          <section className="new-quote">
-            <form onSubmit={getNewQuote}>
-              <button type="submit">Get quote</button>
-            </form>
+        <div className="quote-container" id="quote-box">
+          <section className="quote" id="text">
+            {quote}
+          </section>
+          <section className="author" id="author">
+            &mdash; {author}
+          </section>
+          <section className="quote-footer">
+            <section className="social-media">
+              <a
+                id="tweet-quote"
+                href={`https://twitter.com/intent/tweet?text="${quote} ${author}`}
+                class="twitter-share-button"
+                data-dnt="true"
+                data-show-count="false">
+                Tweet
+              </a>
+            </section>
+            <section className="new-quote" id="new-quote">
+              <form onSubmit={getNewQuote}>
+                <button type="submit">New quote</button>
+              </form>
+            </section>
           </section>
         </div>
       </div>
